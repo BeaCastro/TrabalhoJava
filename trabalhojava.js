1f73aa659732bd74f9cbfced67e65933c062b14a
Beatriz Castro <lsantos2733@hotmail.com.br>
2019-03-22 13:02:52 +0000

    Adicionada mais uma função

--- trabalhojava.js
+++ trabalhojava.js
@@ -7,3 +7,13 @@
         obj.style.backgroundColor = "red";
 }
 
+function retirar(){
+    var lis = document.querySelectorAll("li");
+    for(var i=0 ; i < lis.length; i++){
+        var obj = document.querySelector("#senha");
+        if(obj.value > 0){
+        lis[i].style.display = "none";
+    }
+        
+    }
+}
